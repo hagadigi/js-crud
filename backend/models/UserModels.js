e84cb545dc4e4189d516706ee86e28f10a5e0174
Hadi Cahyadi <hadicahyadi456@gmail.com>
2023-05-25 17:35:50 +0700

    integrating db

---
+++ backend/models/UserModels.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+import db from '../config/database.js';
+
+const {DataTypes} = Sequelize;
+
+const User = db.define('users',{
+  firstName: DataTypes.STRING,
+  lastName: DataTypes.STRING,
+  phoneNumber: DataTypes.STRING
+},{
+  freezeTableName:true
+});
+
+export default User;
+
+(async()=>{
+  await db.sync();
+})();
